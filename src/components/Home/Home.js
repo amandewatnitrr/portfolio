bc745faa5f2a330c8a5e6ac9256e61bde6ef4989
amandewatnitrr <amandewatnitrr@gmail.com>
2023-02-19 18:30:04 +0530

    Minor Changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,6 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
-import geimg from "../../Assets/ge-htc.png"
 
 function Home() {
   return (
